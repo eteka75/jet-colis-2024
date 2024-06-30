45a211e0a0f40a64d48ab0c05552f3459be6748a
ETEKA Wilfried <etekawilfried@gmail.com>
2024-06-30 03:59:08 +0200

    OAuth with google is ready

--- src/i18n.js
+++ src/i18n.js
@@ -5,8 +5,8 @@ import { getCookie } from 'cookies-next';
 export default getRequestConfig(async (req) => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
-  //const locale = 'fr';
-  const locale = getLanguageCookie(req) || 'fr';
+  const locale = 'fr';
+  // const locale = getLanguageCookie(req) || 'fr';
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default,
