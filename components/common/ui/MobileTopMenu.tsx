69a627dae753ec9f535bf317156fbd06e21f1827
ETEKA Wilfried <etekawilfried@gmail.com>
2024-07-15 13:43:38 +0200

    Add top Menu Sheet

--- components/common/ui/MobileTopMenu.tsx
+++ components/common/ui/MobileTopMenu.tsx
@@ -5,16 +5,37 @@ import logoLight from '@/public/assets/images/logo-v0.png';
 import logoDark from '@/public/assets/images/logo-v1.png';
 import Image from 'next/image';
 import { CgMenuLeft } from 'react-icons/cg';
-import { Bell, Lock, Settings, Shield, UserIcon } from 'lucide-react';
+import {
+  Bell,
+  Heart,
+  HelpCircle,
+  Info,
+  Lock,
+  LogOut,
+  MessageCircle,
+  Plus,
+  Search,
+  SearchIcon,
+  Settings,
+  Shield,
+  UserCheck,
+  UserIcon,
+  UserPlus,
+} from 'lucide-react';
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTrigger,
 } from '@/components/ui/sheet';
+import { Separator } from '@/components/ui/separator';
+import { PiPackageDuotone } from 'react-icons/pi';
+import { CiHeart } from 'react-icons/ci';
+import SwithtTheme from '../Header/SwithtTheme';
 const MobileTopMenu = () => {
   const linkStyles =
-    'flex items-center gap-2 text-neutral-500 dark:text-white/80 px-4 py-2 rounded-md hover:bg-accent dark:hover:text-white hover:no-underline';
+    'flex items-center hover:text-primary gap-2 text-neutral-500 dark:text-white/80  py-2 rounded-md  dark:hover:text-white hover:no-underline';
 
   return (
     <div>
@@ -27,47 +48,93 @@ const MobileTopMenu = () => {
           </div>
         </SheetTrigger>
 
-        <SheetContent side={'left'} className="w-[400px] sm:w-[540px]">
+        <SheetContent side={'left'} className="w-[400px] p-0 sm:w-[540px]">
           <SheetHeader>
-            <div className="pb-4">
+            <div className="pb-2 border-b p-4">
               <Image
                 src={logoLight}
                 alt="Colistify"
-                className="h-8 lg:h-9 xl:h-9 w-auto  dark:hidden"
+                className="h-7 lg:h-9 xl:h-9 w-auto  dark:hidden"
               />
               <Image
                 src={logoDark}
                 alt="Colistify"
-                className="h-9 lg:h-10 xl:h-10 w-auto hidden dark:flex"
+                className="h-7 lg:h-10 xl:h-10 w-auto hidden dark:flex"
               />
             </div>
           </SheetHeader>
-          <div className="py-4">
+
+          <div>
             <aside>
-              <ul className="space-y-1 ">
+              <ul className=" text-sm border-b px-4">
                 <li>
-                  <Link href="/user/profil" className={linkStyles}>
-                    <UserIcon className="w-5 h-5" /> Profile
+                  <div className="flex gap-4 items-center justify-between">
+                    <div>Changer de thème</div>
+                    <div className="text-end">
+                      <SwithtTheme />
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <ul className="space-y-1 text-sm border-b p-4">
+                <li>
+                  <Link href="/new-travel" className={linkStyles}>
+                    <Plus className="w-5 h-5" /> Plublier une offre
                   </Link>
                 </li>
                 <li>
-                  <Link href="/user/settings" className={linkStyles}>
-                    <Settings className="w-5 h-5" /> Account Settings
+                  <Link href="/search" className={linkStyles}>
+                    <Search className="w-5 h-5" /> Rechercher
+                  </Link>
+                </li>
+              </ul>
+              <ul className="space-y-1 text-sm border-b p-4">
+                <li>
+                  <Link href="/user/messages" className={linkStyles}>
+                    <MessageCircle className="w-5 h-5" /> Messages
                   </Link>
                 </li>
                 <li>
-                  <Link href="/user/security" className={linkStyles}>
-                    <Lock className="w-5 h-5" /> Security
+                  <Link href="/user/favoris" className={linkStyles}>
+                    <Heart className="w-5 h-5" /> Favoris
+                  </Link>
+                </li>
+
+                <li>
+                  <Link href="/signin" className={linkStyles}>
+                    <UserCheck className="w-5 h-5" /> Se connecter
                   </Link>
                 </li>
                 <li>
-                  <Link href="/user/notifications" className={linkStyles}>
-                    <Bell className="w-5 h-5" /> Notifications
+                  <Link href="/signup" className={linkStyles}>
+                    <UserPlus className="w-5 h-5" /> Créer un compte
                   </Link>
                 </li>
                 <li>
-                  <Link href="/user/privacy" className={linkStyles}>
-                    <Shield className="w-5 h-5" /> Privacy
+                  <Link href="/search/saved" className={linkStyles}>
+                    <SearchIcon className="w-5 h-5" /> Recherches sauvegardés
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="#"
+                    className={linkStyles + ' text-red-500 font-bold'}
+                  >
+                    <LogOut className="w-5 h-5" /> Déconnexion
+                  </Link>
+                </li>
+              </ul>
+            </aside>
+            <aside>
+              <ul className="space-y-1 text-sm border-b p-4">
+                <li>
+                  <Link href="/help" className={linkStyles}>
+                    <HelpCircle className="w-5 h-5" /> Aide
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/faq" className={linkStyles}>
+                    <Info className="w-5 h-5" /> FAQ
                   </Link>
                 </li>
               </ul>
