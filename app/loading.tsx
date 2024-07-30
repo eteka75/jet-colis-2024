f28fa0ec529743220fca95137d799c5f6730f8c4
ETEKA Wilfried <etekawilfried@gmail.com>
2024-07-30 23:40:08 +0200

    Save deleted IdeO

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const loading = () => {
   return (
     <div className="flex items-center justify-center py-40 h-screen bg-accente">
-      {/* <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center">
         <div className="w-16 h-16 border-4 border-primary border-t-transparent border-solid rounded-full animate-spin"></div>
         <p className="mt-4 opacity-80">Chargement...</p>
-      </div> */}
+      </div>
     </div>
   );
 };
